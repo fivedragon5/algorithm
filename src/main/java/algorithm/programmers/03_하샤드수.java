c6eb8a61047aeb4e17d248f28d48239af7930c66
오준용 <ojy@curioud.com>
2022-04-26 17:51:44 +0900

    연습문제 테스트

---
+++ src/main/java/algorithm/programmers/03_하샤드수.java
@@ -0,0 +1,22 @@
+package algorithm.programmers;
+
+class Lesson12947 {
+    static boolean solution(int x) {
+        String numberStr = String.valueOf(x);
+
+        int sum = 0;
+        for(int i = 0; i < numberStr.length() ; i ++)
+            sum += Integer.parseInt(String.valueOf(numberStr.charAt(i)));
+
+        if(x%sum == 0) return true;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println("===START===");
+        System.out.println(solution(number));
+        System.out.println("===END===");
+    }
+}
---
+++ src/main/java/algorithm/programmers/04_최대공약수와최소공배수.java
@@ -0,0 +1,30 @@
+package algorithm.programmers;
+
+class Lesson12940 {
+    static int[] solution(int n, int m) {
+        int gcd = gcd(n, m);
+        int[] answer = {gcd, lcm(n,m,gcd)};
+        return answer;
+    }
+
+    static int gcd(int a, int b) {
+        while(b > 0) {
+            int r = b;
+            b = a % b;
+            a = r;
+        }
+        return a;
+    }
+
+    static int lcm(int a, int b, int gcd) {
+        return ( a * b ) / gcd;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int m = 30;
+        System.out.println("===START===");
+        System.out.println(solution(n, m));
+        System.out.println("===END===");
+    }
+}
