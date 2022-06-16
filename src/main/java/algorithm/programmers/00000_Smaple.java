ac11289169af8cbf3cc5e1cd15bd62dd97026d2c
오준용 <ojy@curioud.com>
2022-06-16 10:45:57 +0900

    뉴스 클러스터링 풀이 참조 추가

--- src/main/java/algorithm/programmers/00000_Smaple.java
+++ src/main/java/algorithm/programmers/00000_Smaple.java
@@ -6,14 +6,30 @@ import java.util.List;
 class Solution {
     static String solution(String parameter) {
         String answer = "";
+        List<Integer> a = new ArrayList<>();
+        a.add(0);
+        a.add(1);
+        a.add(2);
+        a.add(3);
+
+//        a.forEach(c -> System.out.println(c));
+
+        LambTest lambTest = (c, d) -> c+d;
+
         return answer;
+    }
+
+    @FunctionalInterface
+    interface LambTest {
+        public int sum(int a, int b);
     }
 
     public static void main(String[] args) {
         String parameter = "";
-        Long start = System.currentTimeMillis();
+        System.out.println("===START===");
+
         System.out.println(solution(parameter));
-        Long end = System.currentTimeMillis();
-        System.out.println("===END=== time:" + (start - end) + "ms");
+
+        System.out.println("===END===");
     }
 }
--- src/main/java/algorithm/programmers/64065_튜플.java
+++ src/main/java/algorithm/programmers/64065_튜플.java
@@ -1,13 +1,9 @@
 package algorithm.programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 class Lesson64065 {
     static int[] solution(String s) {
-        List<Integer> list = new ArrayList<>();
         HashMap<Integer, String> tupleList = new HashMap<>();
         int left = 1;
         int right = 1;
@@ -18,35 +14,32 @@ class Lesson64065 {
                 left = i;
             }
             else if (ch == '}') {
-                //수정 필요 length X
                 right = i;
                 String tuple = s.substring(left+1, right);
-                tupleList.put((tuple.length()/2)+1, tuple);
+                tupleList.put((tuple.split(",").length), tuple);
             }
         }
-
-        System.out.println(tupleList.toString());
 
         int[] answer = new int[tupleList.size()];
 
         for(int i = 1 ; i <= tupleList.size() ; i ++) {
-            answer[i-1] = Integer.parseInt(tupleList.get(i).replaceAll(",",""));
+            String temp = tupleList.get(i).replaceAll(",","");
+            answer[i-1] = Integer.parseInt(temp);
             for(int j = i+1 ; j <= tupleList.size() ; j ++) {
-                String a = tupleList.get(j);
-                String b = a.replace(tupleList.get(i), "");
-                tupleList.put(j, b);
+                //tupleList.put(j, tupleList.get(j).replaceFirst(Integer.toString(answer[i-1]), ""));
+                tupleList.put(j, tupleList.get(j).replaceFirst(temp, ""));
             }
         }
-
-        System.out.println(Arrays.toString(answer));
-
         return answer;
-
     }
+
+    
+
+
 
     public static void main(String[] args) {
         //2, 3, 1, 2
-        //String s = "{{1,3,2},{2},{2,1,3,2},{3,2}}";
+        //String s = "{{1,2,3},{2,1},{1,2,4,3},{2}}";
         String s = "{20,111},{111}}";
         System.out.println("===START===");
         System.out.println(solution(s));
