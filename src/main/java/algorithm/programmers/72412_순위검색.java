816bc8d424e636076e86a3ea94dc4485ecdfb132
OJY <ojy9406@naver.com>
2022-06-19 22:59:56 +0900

    1

--- src/main/java/algorithm/programmers/72412_순위검색.java
+++ src/main/java/algorithm/programmers/72412_순위검색.java
@@ -7,12 +7,12 @@ import java.util.stream.Collectors;
 
 class Lesson72412 {
     static int[] solution(String[] info, String[] query) {
-        int[] answer = {};
+        int[] answer = new int[query.length];
         List<Developer> developersList = new ArrayList<>();
+        int index = 0;
 
         for (String result : info) {
             String[] splitStr = result.split(" ");
-            System.out.println(splitStr[4]);
             Developer developer = new Developer(splitStr[0], splitStr[1], splitStr[2], splitStr[3], Integer.parseInt(splitStr[4]));
             developersList.add(developer);
         }
@@ -20,24 +20,35 @@ class Lesson72412 {
         //점수순 정렬
         developersList.stream().sorted(Comparator.comparing(Developer::getPoint)).collect(Collectors.toList());
 
+        //System.out.println(developersList.toString());
+
         for (String search : query) {
             String[] searchStr = search.split(" and | ");
             int point = Integer.parseInt(searchStr[4]);
-            int startPointer = startPointer(point, developersList);
+            int filteredDevelopers = startPointer(developersList, point, searchStr[0], searchStr[1], searchStr[2], searchStr[3]);
+            answer[index++] = filteredDevelopers;
         }
-
+        //System.out.println(Arrays.toString(answer));
         return answer;
     }
     
     // point 이상부터 탐색할것
     // filter 사용해보기
     // '-'처리 filter로 가능한가
-    static int startPointer(int point, List<Developer> developers) {
+    static int startPointer(List<Developer> developers, int point, String language, String position, String rank, String soulFood) {
         int result = 0;
-        List<Developer> a = developers.stream().filter(vo -> vo.getLanguage().equals("java")).collect(Collectors.toList());
-        System.out.println(a.toString());
-        System.out.println("===============");
-        return 0;
+        //System.out.println("언어 : " + language + " / 포지션 : " + position + " / 직급 : " + rank + " / 소울푸드 :" + soulFood);
+        //(효율성 TEST FAIL)
+        //filter하기전 list의 탐색 범위를 줄인뒤 탐색하기
+        List<Developer> filterDevelopers =
+                developers.stream()
+                        .filter(vo -> (language.equals("-") || vo.getLanguage().equals(language)))
+                        .filter(vo -> (position.equals("-") || vo.getPosition().equals(position)))
+                        .filter(vo -> (rank.equals("-") || vo.getRank().equals(rank)))
+                        .filter(vo -> (soulFood.equals("-") || vo.getSoulFood().equals(soulFood)))
+                        .filter(vo -> vo.getPoint() >= point)
+                        .collect(Collectors.toList());
+        return filterDevelopers.size();
     }
     
     //개발자 클래스
@@ -84,13 +95,21 @@ class Lesson72412 {
                     ", rank='" + rank + '\'' +
                     ", soulFood='" + soulFood + '\'' +
                     ", point=" + point +
-                    '}';
+                    '}' + "\n";
         }
     }
 
     public static void main(String[] args) {
         String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
-        String[] query = {"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
+        String[] query =
+                {
+                "java and backend and junior and pizza 100",
+                "python and frontend and senior and chicken 200",
+                "cpp and - and senior and pizza 250",
+                "- and backend and senior and - 150",
+                "- and - and - and chicken 100",
+                "- and - and - and - 150"
+                };
         //result 1,1,1,1,2,4
         System.out.println("===START===");
         System.out.println(solution(info, query));
