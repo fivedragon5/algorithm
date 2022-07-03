4177fad0de5b06c7d0d94f901fbd5c9d99e98853
OJY <ojy9406@naver.com>
2022-07-04 00:16:30 +0900

    무튜브 커밋

--- src/main/java/algorithm/backjoon/study/15591_MooTube.java
+++ src/main/java/algorithm/backjoon/study/15591_MooTube.java
@@ -3,33 +3,65 @@ package algorithm.backjoon.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Problem15591 {
-    private static class Video {
-        int num;
-        int usado;
 
-        public Video(int num, int usado) {
-            this.num = num;
-            this.usado = usado;
-        }
+    static int n, q;
 
-    }
     public static void main(String srgs[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         int q = Integer.parseInt(st.nextToken());
+
+        ArrayList<int[]>[] usado  = new ArrayList[n+1];
+
+        for(int i =1; i < n+1; i++) {
+            usado[i] = new ArrayList<int[]>();
+        }
 
         for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b =  Integer.parseInt(st.nextToken());
             int c =  Integer.parseInt(st.nextToken());
+
+            usado[a].add(new int[]{b, c});
+            usado[b].add(new int[]{a, c});
         }
-        System.out.print(sb);
+
+        for(int i = 1; i <= q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int answer = 0;
+
+            int k = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            Queue<Integer> queue = new LinkedList<>();
+
+            int[] visited = new int[n+1];
+            visited[v] = 1;
+            queue.add(v);
+
+            while(!queue.isEmpty()) {
+                int now = queue.poll();
+                for(int[] arr : usado[now] ) {
+                    int next = arr[0];
+                    int usadoMin = arr[1];
+                    if(visited[next] == 1 || usadoMin < k)
+                        continue;
+                    answer++;
+                    visited[next] = 1;
+                    queue.add(next);
+                }
+            }
+            System.out.println(answer);
+        }
     }
 }
