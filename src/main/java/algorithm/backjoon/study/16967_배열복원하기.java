a101b7c221a368dec2e98931fbd61ce0404f6160
fivedragons5 <ojy9406@naver.com>
2022-07-07 10:56:30 +0900

    배열 복원하기 1차 정답 커밋

--- src/main/java/algorithm/backjoon/study/16967_배열복원하기.java
+++ src/main/java/algorithm/backjoon/study/16967_배열복원하기.java
@@ -31,20 +31,47 @@ class Problem16967 {
             }
         }
 
-        for (int i = 0; i < H + X ; i++) {
-            for (int j = 0; j < W + Y ; j++) {
-                if (i < X || j < Y) {
-                    if(i < H && j < W) {
-                        arrayA[i][j] = arrayB[i][j];
-                    }
-                }
-                if (i >= H || j >= W) {
-                    if (i-X >= 0 && j-Y >= 0) {
-                        arrayA[i-X][j-Y] = arrayB[i][j];
-                    }
-                }
+        //1. X,Y만큼 이동해도 관련없는 값들을 먼저 채운다.
+        //2. 먼저 채운값들을 제외한 나머지 부분의 for문을 돌린다.
+
+        //1.
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < arrayA[i].length; j++) {
+                arrayA[i][j] = arrayB[i][j];
             }
         }
+
+        for (int i = 0; i < Y; i++) {
+            for (int j = 0; j < arrayA.length; j++) {
+                arrayA[j][i] = arrayB[j][i];
+            }
+        }
+
+        // 2.
+        for (int i = X; i < arrayA.length; i++) {
+            for (int j = Y; j < arrayA[i].length; j++) {
+                arrayA[i][j] = arrayB[i][j] - arrayA[i-X][j-Y];
+            }
+        }
+
+        /**
+         * 최초풀이
+         */
+//        for (int i = 0; i < H + X ; i++) {
+//            for (int j = 0; j < W + Y ; j++) {
+//                if (i < X || j < Y) {
+//                    if(i < H && j < W) {
+//                        arrayA[i][j] = arrayB[i][j];
+//                    }
+//                }
+//                if (i >= H || j >= W) {
+//                    if (i-X >= 0 && j-Y >= 0) {
+//                        arrayA[i-X][j-Y] = arrayB[i][j];
+//                    }
+//                }
+//            }
+//        }
+
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
                 System.out.print(arrayA[i][j]);
