00f2fe27ac9b5cc48d3894e19d93b850e83e8ce2
OJY <ojy9406@naver.com>
2022-08-23 00:07:00 +0900

    풍선터트리기, 쿼드압축 추가

---
+++ src/main/java/algorithm/code/programmers/68646_풍선터트리기.java
@@ -0,0 +1,16 @@
+package algorithm.code.programmers;
+
+class Lesson68646 {
+    static int solution(int[] a) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {9,-1,-5}; // 3
+        //int[] a = {-16,27,65,-2,58,-92,-71,-68,-61,-33}; // 6
+        System.out.println("===START===");
+        System.out.println(solution(a));
+        System.out.println("===END===");
+    }
+}
---
+++ src/main/java/algorithm/code/programmers/68936_쿼드압축후개수세기.java
@@ -0,0 +1,29 @@
+package algorithm.code.programmers;
+
+class Lesson68936 {
+    static int[] solution(int[][] arr) {
+        int[] answer = {};
+
+        //제곱 수
+        int n = arr.length;
+
+        for(int i = n; i > 0; i--) {
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        /*
+            1. arr의 행의 개수는 1 이상 1024 이하이며, 2의 거듭 제곱수 형태를 하고 있습니다. 즉, arr의 행의 개수는 1, 2, 4, 8, ..., 1024 중 하나입니다.
+            2. arr의 각 행의 길이는 arr의 행의 개수와 같습니다. 즉, arr은 정사각형 배열입니다.
+            3. arr의 각 행에 있는 모든 값은 0 또는 1 입니다
+         */
+        int[][] arr = {{1,1,0,0},{1,0,0,0},{1,0,0,1},{1,1,1,1}}; // [4,9]
+        //int[][] arr = {{1,1,1,1,1,1,1,1},{0,1,1,1,1,1,1,1},{0,0,0,0,1,1,1,1},{0,1,0,0,1,1,1,1},{0,0,0,0,0,0,1,1},{0,0,0,0,0,0,0,1},{0,0,0,0,1,0,0,1},{0,0,0,0,1,1,1,1}}; // [10,15]
+        System.out.println("===START===");
+        System.out.println(solution(arr));
+        System.out.println("===END===");
+    }
+}
