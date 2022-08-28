42593f1742311699c4c15b8a5d4514011575b47f
OJY <ojy9406@naver.com>
2022-08-29 00:45:30 +0900

    빛의경로싸이클 추가

---
+++ src/main/java/algorithm/code/programmers/86052_빛의경로싸이클.java
@@ -0,0 +1,15 @@
+package algorithm.code.programmers;
+
+class Lesson86052 {
+    static int[] solution(String[] grid) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] grid = {};
+        System.out.println("===START===");
+        System.out.println(solution(grid));
+        System.out.println("===END===");
+    }
+}
