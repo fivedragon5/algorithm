e1728dd3ebbb03450f34a44222bfe408e0842993
fad <ojy9406@naver.com>
2022-10-05 16:49:54 +0900

    넴모넴모2020 커밋

--- src/main/java/algorithm/code/study/19845_넴모넴모2020.java
+++ src/main/java/algorithm/code/study/19845_넴모넴모2020.java
@@ -29,62 +29,60 @@ class Problem19845 {
 
         N = Integer.parseInt(st.nextToken());
         int Q = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
 
         squares = new int[N];
-        
+
         //네모 입력
         for (int i = 0; i < N; i++) {
             squares[i] = Integer.parseInt(st.nextToken());
         }
 
-        //레이저 계획 시작
+        int x,y,indexY;
+
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
-            int laser_x = Integer.parseInt(st.nextToken());
-            int laser_y = Integer.parseInt(st.nextToken());
-            if(N <= laser_y) {
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            //우측에 넴모가 없을경우 추가
+            if (squares[y-1] < x) {
                 System.out.println(0);
                 continue;
             }
-            else {
-                int remove_x = squares[laser_y - 1] - laser_x + 1;
-                System.out.println("square:" + squares[laser_y-1] + "/laser_x:" + laser_x + "/remove:" + remove_x);
-                int find_y = findRemoveY(laser_x, laser_y);
-                int remove_y = N - laser_y + 1 - (N - find_y);
-                System.out.println(remove_x + remove_y - 1);
+            if (squares[N-1] < x) {
+                indexY = findIndexY(x, y);
             }
+            else {
+                indexY = N;
+            }
+            System.out.println(squares[y-1] - x + indexY - y + 1);
         }
     }
 
-    static int findRemoveY(int x, int y) {
-        // squares[n] = x-1 && squares[n-1] = x 를 만족하는 n을 찾는다.
+    /**
+     * 이분탐색
+     */
+    static int findIndexY(int x, int y) {
         int left = 0;
-        int right = N-1;
+        int right = N;
         int middle = 0;
-
-        System.out.println("===findY start===");
-
-        while (left <= right) {
-
+        while (left < right) {
             middle = (left + right) / 2;
-
-            System.out.println("left:" + left + "/right:" + right + "/middle:" + middle);
-
             if (squares[middle] < x) {
                 right = middle;
             }
             else {
-                left = middle;
+                left = middle + 1;
             }
         }
-
-        return middle;
+        return right;
     }
 }
 
 /**
-
+ *
  oo
  ooo
  ooo
