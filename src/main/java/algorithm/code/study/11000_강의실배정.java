31e962986926aca27d57c28927c51e5d5ec37f93
OJY <ojy9406@naver.com>
2022-10-27 23:20:33 +0900

    강의실 배정 커밋

--- src/main/java/algorithm/code/study/11000_강의실배정.java
+++ src/main/java/algorithm/code/study/11000_강의실배정.java
@@ -3,6 +3,8 @@ package algorithm.code.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Problem11000 {
@@ -26,12 +28,45 @@ class Problem11000 {
             list[i][0] = Integer.parseInt(st.nextToken());
             list[i][1] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(list, (m1, m2) -> {
+            if (m1[0] == m2[0]) {
+                return m1[1] - m2[1];
+            }
+            return m1[0] - m2[0];
+        });
+
+        solve2(list);
+
+
+//        for(int[] a : list) {
+//            System.out.println(Arrays.toString(a));
+//        }
+
+    }
+
+    public static void solve2(int[][] list) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        int room = 0;
+
+        queue.add(list[0][1]);
+
+        for (int i = 1; i < list.length; i++) {
+            while (!queue.isEmpty() && queue.peek() <= list[i][0]) {
+                queue.poll();
+            }
+            queue.add(list[i][1]);
+            room = queue.size() > room ? queue.size() : room;
+        }
+
+        System.out.println(room);
     }
 }
 
 /**
  3
+ 3 5
  1 3
  2 4
- 3 5
  */
