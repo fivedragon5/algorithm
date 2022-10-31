be33079dc87ed3d03f28f970f9279c9fff163c9e
OJY <ojy9406@naver.com>
2022-10-31 23:22:56 +0900

    테트로미노 커밋

--- src/main/java/algorithm/code/study/14500_테트로미노.java
+++ src/main/java/algorithm/code/study/14500_테트로미노.java
@@ -3,9 +3,15 @@ package algorithm.code.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 class Problem14500 {
+
+    static int MAX = 0;
+    static int N, M; //y, x
+    static int[][] map;
 
     /**
       4 ≤ N, M ≤ 500
@@ -17,20 +23,52 @@ class Problem14500 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[][] array = new int[N][M];
-
+        map = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++) {
-                array[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+
+        for (int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                int sum = map[i][j];
+                List<String> list = new LinkedList<>();
+                list.add("(" + j + "," + i + ")");
+                solve(j, i, 1, sum, list);
+            }
+        }
+
+        System.out.println("a"+MAX);
+
         
-        //시작점을 기준으로 우측 or 아래로 탐색하기마
+    }
+
+    static void solve(int x, int y, int step, int sum, List<String> list) {
+
+        if (list.size() == 4) {
+            System.out.println(sum + "/" + list.toString());
+            MAX = Math.max(MAX, sum);
+            return;
+        }
+
+        if (x+1 < M) {
+            List<String> listX = list;
+            listX.add("(" +(x+1) + "," + y + ")");
+            solve(x+1, y, step+1, sum + map[y][x+1], listX);
+        }
+
+        if (y+1 < N) {
+            List<String> listY = list;
+            listY.add("(" + x + "," + (y+1) + ")");
+            solve(x, y+1, step+1, sum + map[y+1][x], listY);
+        }
     }
 }
 
