434fbb0a5373d97192f2bd0a46337717aa5a1d00
fad <ojy9406@naver.com>
2022-11-03 18:24:41 +0900

    나무 탈출 추가

--- src/main/java/algorithm/code/study/15900_나무탈출.java
+++ src/main/java/algorithm/code/study/15900_나무탈출.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Problem15900 {
@@ -21,6 +22,9 @@ class Problem15900 {
 
      */
 
+    static ArrayList<ArrayList<Integer>> roads;
+    static int leafSum = 0;
+
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,7 +32,7 @@ class Problem15900 {
 
         int N = Integer.parseInt(st.nextToken());
 
-        ArrayList<ArrayList<Integer>> roads = new ArrayList<>();
+        roads = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             roads.add(new ArrayList<Integer>());
@@ -36,11 +40,43 @@ class Problem15900 {
 
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            roads.get(Integer.parseInt(st.nextToken())-1).add(Integer.parseInt(st.nextToken())-1);
+            int node1 = Integer.parseInt(st.nextToken()) - 1;
+            int node2 = Integer.parseInt(st.nextToken()) - 1;
+            roads.get(node1).add(node2);
+            roads.get(node2).add(node1);
         }
 
-        for(ArrayList<Integer> al : roads) {
-            System.out.println(al.toString());
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[N];
+
+        // Root Node = 0
+        visited[0] = true;
+        solve(0, stack, visited);
+
+        System.out.println(leafSum%2 == 0 ? "NO" : "YES");
+
+//        for(ArrayList<Integer> al : roads) {
+//            System.out.println(al.toString());
+//        }
+    }
+
+    static public void solve(int node, Stack<Integer> stack, boolean[] visited) {
+
+        //간선을 연결 했을때 차수가1인 노드 = 리프노드
+        if (roads.get(node).size() == 1 && node != 0) {
+            //System.out.println("리프노드 : " + node + "/ size : " + stack.size());
+            leafSum += stack.size();
+        }
+
+        for (int n : roads.get(node)) {
+            //방문 체크
+            if (!visited[n]) {
+                visited[n] = true;
+                stack.add(n);
+                solve(n, stack, visited);
+                stack.pop();
+                visited[n] = false;
+            }
         }
     }
 }
@@ -68,4 +104,23 @@ class Problem15900 {
  2 3
 
  No
+
+ 10
+ 1 2
+ 2 3
+ 3 4
+ 4 5
+ 5 6
+ 5 7
+ 5 8
+ 5 9
+ 5 10
+
+ 1 2
+ 1 3
+ 1 4
+ 1 5
+ 1 6
+ 1 7
+ 1 8
  */
