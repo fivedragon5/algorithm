e59224ae8cf7bc1f8bf27f514c9b659c4a4754fd
fad <ojy9406@naver.com>
2022-11-04 09:53:15 +0900

    정답을 맞춰 놓고도 출력을 YES | NO 대문자로 해서 삽질한 문제 갈아 엎기만 몇시간 ㅠㅠ  - 출력값을 잘 확인하는 습관을 들여보자

--- src/main/java/algorithm/code/study/15900_나무탈출.java
+++ src/main/java/algorithm/code/study/15900_나무탈출.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Problem15900 {
@@ -22,8 +21,9 @@ class Problem15900 {
 
      */
 
-    static ArrayList<ArrayList<Integer>> roads;
+    static ArrayList<ArrayList<Integer>> roads = new ArrayList<>();
     static int leafSum = 0;
+    static boolean[] visited;
 
     public static void main(String args[]) throws IOException {
 
@@ -31,53 +31,40 @@ class Problem15900 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
 
-        roads = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i <= N; i++) {
             roads.add(new ArrayList<Integer>());
         }
 
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int node1 = Integer.parseInt(st.nextToken()) - 1;
-            int node2 = Integer.parseInt(st.nextToken()) - 1;
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
             roads.get(node1).add(node2);
             roads.get(node2).add(node1);
         }
+        dfs(1,0);
 
-        Stack<Integer> stack = new Stack<>();
-        boolean[] visited = new boolean[N];
-
-        // Root Node = 0
-        visited[0] = true;
-        solve(0, stack, visited);
-
-        System.out.println(leafSum%2 == 0 ? "NO" : "YES");
-
-//        for(ArrayList<Integer> al : roads) {
-//            System.out.println(al.toString());
-//        }
+        if (leafSum%2 != 0) System.out.println("Yes");
+        else System.out.println("No");
     }
 
-    static public void solve(int node, Stack<Integer> stack, boolean[] visited) {
+    static void dfs(int node, int step) {
 
-        //간선을 연결 했을때 차수가1인 노드 = 리프노드
-        if (roads.get(node).size() == 1 && node != 0) {
-            //System.out.println("리프노드 : " + node + "/ size : " + stack.size());
-            leafSum += stack.size();
-        }
+        visited[node] = true;
 
-        for (int n : roads.get(node)) {
-            //방문 체크
-            if (!visited[n]) {
-                visited[n] = true;
-                stack.add(n);
-                solve(n, stack, visited);
-                stack.pop();
-                visited[n] = false;
+        for (int num : roads.get(node)) {
+
+            if (!visited[num]) {
+                dfs(num, step+1);
             }
         }
+
+        if (roads.get(node).size() == 1) {
+            leafSum += step;
+        }
+
     }
 }
 
@@ -116,6 +103,7 @@ class Problem15900 {
  5 9
  5 10
 
+ 7
  1 2
  1 3
  1 4
