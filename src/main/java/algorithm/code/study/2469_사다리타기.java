274eae37d9501cb3bbc9559b7dd00349081f2c95
OJY <ojy9406@naver.com>
2022-11-10 22:31:07 +0900

    사다리 타기 커밋

--- src/main/java/algorithm/code/study/2469_사다리타기.java
+++ src/main/java/algorithm/code/study/2469_사다리타기.java
@@ -3,19 +3,27 @@ package algorithm.code.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Problem2469 {
 
     /**
+     * 풀이)
+     *  숨겨저 있는 사다리를 기준으로 시작선부터 숨겨저있는 줄전까지의 순서를 구하고
+     *  최종순서에서부터 숨겨저 있는 줄까지의 순서를 구한다음 비교해서 사다리를 생성해준다.
      *
+     * 설명)
+     * ladders[][] : 사다리 위치
+     * questionMarkLine : 숨겨저 있는 라인 
+     * movePlayer() : 사람들 이동
      *
      * 첫 줄에는 참가한 사람의 수 k가 나온다(3 ≤ k ≤ 26).
      * 그 다음 줄에는 가로 막대가 놓일 전체 가로 줄의 수를 나타내는 n이 나온다(3 ≤ n ≤ 1,000).
      * 그리고 세 번째 줄에는 사다리를 타고 난 후 결정된 참가자들의 최종 순서가 길이 k인 대문자 문자열로 들어온다.
      * k와 n, 그리고 도착순서 문자열이 나타난 다음, 이어지는 n개의 줄에는 앞서 설명한 바와 같이 ‘*’와 ‘-’ 문자로 이루어진 길이 k-1인 문자열이 주어진다.
      * 그 중 감추어진 가로 줄은 길이가 k-1인 ‘?’ 문자열로 표시되어 있다.
+     *
+     * 사다리를 놓아도 순서를 맞출 수 없는경우 k-1 만큼 "x"를 출력 해준다. *
      */
     static int k,n;
     static String ladders[][];
@@ -62,15 +70,14 @@ class Problem2469 {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0 ; i < k - 1 ; i++) {
-            if(playersToAscii[i] != resultPlayersToAscii[i]) {
+            if(playersToAscii[i] != resultPlayersToAscii[i] && playersToAscii[i] != resultPlayersToAscii[i+1]) {
+                System.out.println("x".repeat(k-1));
+                return;
+            }
+            else if(playersToAscii[i] != resultPlayersToAscii[i]) {
                 sb.append("-");
                 sb.append("*");
                 i++;
-            }
-            else if(playersToAscii[i] != resultPlayersToAscii[i] && playersToAscii[i+1] != resultPlayersToAscii[i+1]) {
-                System.out.println("x".repeat(k-1));
-                System.out.println("@@@");
-                return;
             }
             else {
                 sb.append("*");
