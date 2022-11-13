e3975294e6d802ea7f0e5bfca0a1eb76d7fc416d
OJY <ojy9406@naver.com>
2022-11-13 23:17:05 +0900

    우박수열적분 커밋

---
+++ src/main/java/algorithm/code/programmers/134239_우박수열정적분.java
@@ -0,0 +1,24 @@
+package algorithm.code.programmers;
+
+/**
+ * 1-1. 입력된 수가 짝수라면 2로 나눕니다.
+ * 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+ * 2.결과로 나온 수가 1보다 크다면 1번 작업을 반복합니다.
+ */
+
+class Lesson134239 {
+    static double[] solution(int k, int[][] ranges) {
+        double[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int k = 5;
+        int[][] ranges = {{0,0},{0,-1},{2,-3},{3,-3}};
+        System.out.println("===START===");
+        System.out.println(solution(k, ranges));
+        System.out.println("===END===");
+    }
+}
