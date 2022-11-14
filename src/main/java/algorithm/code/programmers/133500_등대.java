bd63da78281abcaa607f6c7366ff3c575cf72fc3
OJY <ojy9406@naver.com>
2022-11-14 19:00:46 +0900

    등대 ...

--- src/main/java/algorithm/code/programmers/133500_등대.java
+++ src/main/java/algorithm/code/programmers/133500_등대.java
@@ -1,7 +1,6 @@
 package algorithm.code.programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 
 class Lesson133500 {
     /**
@@ -15,16 +14,19 @@ class Lesson133500 {
      */
 
     static int LIGHT_HOUSE_COUNT;
-    static ArrayList<ArrayList<Integer>> course = new ArrayList<>();
+    static LinkedList<LinkedList<Integer>> course = new LinkedList<>();
     static int answer;
+    static boolean[] isLightOn;
 
     static int solution(int n, int[][] lighthouse) {
 
-        boolean[] isLightOn = new boolean[n];
+        answer = n/2;
+
+        isLightOn = new boolean[n];
         LIGHT_HOUSE_COUNT = n;
 
         for (int i = 0; i < n ; i++) {
-            course.add(new ArrayList<>());
+            course.add(new LinkedList<>());
         }
 
         for (int i = 0; i < n-1; i++) {
@@ -32,16 +34,9 @@ class Lesson133500 {
             course.get(lighthouse[i][1]-1).add(lighthouse[i][0]-1);
         }
 
-        int leafNodeCount = 0;
-
-        for (ArrayList c : course) {
-            if (c.size() > 1) {
-                leafNodeCount++;
-            }
-        }
-
-        for (int i = 1 ; i <= n - leafNodeCount ; i ++) {
-            if (turnOnLignt(i, 0, isLightOn)) {
+        //최소 등의 갯수 1 <= onLightHouse <= 등대 수 - leafNode
+        for (int i = 1; i <= answer; i ++) {
+            if (turnOnLignt(i, 0, 0)) {
                 break;
             }
         }
@@ -49,30 +44,31 @@ class Lesson133500 {
         return answer;
     }
 
-    static boolean turnOnLignt(int turnOnlightCount, int count, boolean[] isLightOn) {
+    static boolean turnOnLignt(int turnOnlightCount, int count, int currentLightHouse) {
 
-        if (turnOnlightCount <= count) {
-            System.out.println(Arrays.toString(isLightOn));
-            if (isPossible(isLightOn)) {
-                answer = Math.max(answer, count);
+        if (turnOnlightCount == count) {
+            //System.out.println(count + "/" + Arrays.toString(isLightOn));
+            if (isPossible()) {
+                answer = count;
                 return true;
             }
+            return false;
         }
 
-        for (int i = 0; i < LIGHT_HOUSE_COUNT; i++) {
-            //반복이 많이 돌아가니 다시 짜보자.
+        for (int i = currentLightHouse; i < LIGHT_HOUSE_COUNT; i++) {
             if(!isLightOn[i] && course.get(i).size() > 1) {
                 isLightOn[i] = true;
-                turnOnLignt(turnOnlightCount, count+1, isLightOn);
+                if (turnOnLignt(turnOnlightCount, count+1, i + 1)) {
+                    return true;
+                }
                 isLightOn[i] = false;
             }
         }
-
         return false;
     }
 
     //가능하다면 종료시키자.
-    static boolean isPossible(boolean[] isLightOn) {
+    static boolean isPossible() {
 
         for (int i = 0; i < LIGHT_HOUSE_COUNT; i++) {
             boolean flag = false;
@@ -88,16 +84,19 @@ class Lesson133500 {
                 }
             }
         }
-
         return true;
     }
 
     public static void main(String[] args) {
         System.out.println("===START===");
+//        int n = 2;
+//        int[][] lighthouse = {{1,2}};
 //        int n = 8;
 //        int[][] lighthouse = {{1, 2}, {1, 3}, {1, 4}, {1, 5}, {5, 6}, {5, 7}, {5, 8}};
         int n = 10;
         int[][] lighthouse = {{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}};
+//        int n = 16;
+//        int[][] lighthouse = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 10}, {10, 11}, {11, 12}, {12, 13}, {13, 14}, {14, 15}, {15, 16}};
         System.out.println(solution(n, lighthouse));
         System.out.println("===END===");
     }
