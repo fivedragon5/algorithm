04fe6f78e1f99a262089bf36a454264b1c765daa
fad <ojy9406@naver.com>
2022-11-23 17:35:56 +0900

    코딩스터디 11월 4주차 문제 추가, codility 문제 테스트 및 Sample Template 추가

---
+++ src/main/java/algorithm/codility/Solution.java
@@ -0,0 +1,16 @@
+package algorithm.codility;
+
+/**
+ * Codility Sample template
+ */
+class Solution {
+    static public int solution(int N) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        int a = 1001;
+        System.out.println(solution(a));
+    }
+}
