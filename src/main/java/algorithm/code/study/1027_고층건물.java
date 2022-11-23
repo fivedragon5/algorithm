8106505b80df61c9987945a87dff937e398efa4a
fad <ojy9406@naver.com>
2022-11-23 16:31:27 +0900

    고층문제 풀이 추가

--- src/main/java/algorithm/code/study/1027_고층건물.java
+++ src/main/java/algorithm/code/study/1027_고층건물.java
@@ -6,62 +6,92 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
+ *
+ * TODO: 2022.11.22 다시풀기
+ *
  * 조건)
  * 1 <= N <= 50
  * 1 <= Height <= 1,000,000,000 (10억)
  */
 class Problem1027 {
 
-    static int BULIDING_COUNT;
+    static int N;
     static long[] bulidingHeights;
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BULIDING_COUNT = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
 
-        bulidingHeights = new long[BULIDING_COUNT];
+        bulidingHeights = new long[N];
 
-        for (int i = 0; i < BULIDING_COUNT; i++) {
+        for (int i = 0; i < N; i++) {
             bulidingHeights[i] = Long.parseLong(st.nextToken());
         }
 
         int answer = 0;
 
-        for (int i = 0; i < BULIDING_COUNT; i++) {
-            answer = Math.max(check(i, bulidingHeights[i]), answer);
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(check(i), answer);
         }
 
         System.out.println(answer);
     }
+    
+    //현재 빌딩에서 좌,우를 봣을때 보이는 빌딩의 수
+    static int check(int viewPositionIndex) {
+        long currentBulidingHeight = bulidingHeights[viewPositionIndex];
+        return leftView(viewPositionIndex, currentBulidingHeight) + rightView(viewPositionIndex, currentBulidingHeight);
+    }
+    /* 기울기 = y2 - y1 / x2 - x1 */
 
-    static int check(int index, double height) {
+    //현재 빌딩에서 왼쪽을 봤을때 보이는 빌딩의 수
+    static int leftView(int index, long height) {
+        int count = 0;
+        long currentGradient_x = 0;
+        long currentGradient_y = 0;
+        long minGradient_x = 1;
+        long minGradient_y = -1000000001;
 
-        double maxGradient = -1000000000L;
-        int rightCount = 0;
-        for (int i = index+1; i < BULIDING_COUNT; i++) {
-            long currentheight = bulidingHeights[i];
-            double gradient = (currentheight - height) / (i - index);
-            if (maxGradient < gradient) {
-                rightCount++;
-                maxGradient = gradient;
+        //Max 기울기 갱신, Max 기울기보다 크면 카운트 증가
+        for (int i = index - 1; i >= 0; i--) {
+
+            currentGradient_y = bulidingHeights[i] - height;
+            currentGradient_x = i - index;
+
+            if (minGradient_y * currentGradient_x > minGradient_x * currentGradient_y) {
+                count++;
+                minGradient_y = currentGradient_y;
+                minGradient_x = currentGradient_x;
+            }
+
+        }
+        return count;
+    }
+
+    //현재 빌딩에서 오른쪽을 봤을때 보이는 빌딩의 수
+    static int rightView(int index, long height) {
+        int count = 0;
+        long currentGradient_x = 0;
+        long currentGradient_y = 0;
+        long maxGradient_x = 1;
+        long maxGradient_y = -1000000001;
+
+        for (int i = index + 1; i < N; i++) {
+
+            currentGradient_y = bulidingHeights[i] - height;
+            currentGradient_x = i - index;
+
+            if (maxGradient_y * currentGradient_x < maxGradient_x * currentGradient_y) {
+                count++;
+                maxGradient_y = currentGradient_y;
+                maxGradient_x = currentGradient_x;
             }
         }
-
-        double minGradient = 100000000L;
-        int leftCount = 0;
-        for (int i = index-1;  i >= 0; i--) {
-            long currentheight = bulidingHeights[i];
-            double gradient = (height - currentheight) / (index - i);
-            if (minGradient > gradient) {
-                leftCount++;
-                minGradient = gradient;
-            }
-        }
-        return rightCount + leftCount;
+        return count;
     }
 }
 /*
