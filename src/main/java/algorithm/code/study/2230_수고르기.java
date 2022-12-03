62291dbf611fbe0ce84b0780c85f79cfecc23147
OJY <ojy9406@naver.com>
2022-12-03 14:32:21 +0900

    수 고르기 주석 추가

--- src/main/java/algorithm/code/study/2230_수고르기.java
+++ src/main/java/algorithm/code/study/2230_수고르기.java
@@ -7,7 +7,10 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- *
+ *  투포인터로 알고리즘 사용
+ *  1.주어진 번호를 오름차순으로 정렬
+ *  2.right:1, left:0 부터 시작하여 numbers[right] - numbers[left]로 차를 구함 
+ *  3.두수의 차와 주어진 M을 비교하여 M보다 클경우 left증가, M보다 작을경우 right증가하여 최소값을 갱신해줌
  *
  * 제한)
  *     1 ≤ N ≤ 100,000
