c21c628be954de59b0bb612ec30f4cfdf77cb38d
OJY <ojy9406@naver.com>
2022-12-05 15:14:52 +0900

    달력 문제 풀이 커밋

--- src/main/java/algorithm/code/study/20207_달력.java
+++ src/main/java/algorithm/code/study/20207_달력.java
@@ -3,13 +3,14 @@ package algorithm.code.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
+ * 풀이)
+ * 일정을 길이:365 배열로 나열한뒤 일정이 있는 부분을 +1 입력
+ * 365배열을 순회하면서 0을 만났을경우 연속된 일정이 끝난것으로 판별한뒤 넓이 계산
  *
- *
- *
+ * 제한)
  * (1 ≤ N ≤ 1000)
  * (1 ≤ S ≤ E ≤ 365)
  */
@@ -20,24 +21,38 @@ class Problem20207 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int[][] days = new int[N][2];
+        int[] calendar = new int[366];
+        int start = 0;
+        int end = 0;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            days[i][0] = Integer.parseInt(st.nextToken());
-            days[i][1] = Integer.parseInt(st.nextToken());
-        }
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken());
 
-        Arrays.sort(days, (day1, day2) -> {
-            if (day1[0] == day2[0]) {
-                return day2[1] - day1[1];
+            for (int j = start-1; j <= end-1; j++) {
+                calendar[j] = calendar[j] + 1;
             }
-            return day1[0] - day2[0];
-        });
-
-        for (int[] d : days) {
-            System.out.println(Arrays.toString(d));
         }
+
+        int width = 0;
+        int max = 0;
+        int number = 0;
+        start = 0;
+
+        for (int i = 0; i < 366; i++) {
+            number = calendar[i];
+            if (max > 0 && number == 0) {
+                width += (i - start) * max;
+                max = 0;
+                start = 0;
+            }
+            else if (number > 0) {
+                if (max == 0) start = i;
+                max = Math.max(max, number);
+            }
+        }
+        System.out.println(width);
     }
 }
 /*
@@ -60,5 +75,8 @@ class Problem20207 {
 2 5
 
 36
+
+1
+1 365
  */
 
