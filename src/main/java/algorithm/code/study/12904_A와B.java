fd43761e52e0b991cb71a1a4924fb544ba531c86
OJY <ojy9406@naver.com>
2022-12-16 01:41:37 +0900

    백준 A와 B 문제 풀이 및 설명 커밋

--- src/main/java/algorithm/code/study/12904_A와B.java
+++ src/main/java/algorithm/code/study/12904_A와B.java
@@ -6,7 +6,15 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
+ * 접근)
+ * T -> S로 바꾸기
  *
+ * 풀이)
+ * 1. T의 끝의 글자가 A인경우
+ *  - 'A' 삭제
+ * 2. T의 끝의 글자가 B인경우
+ *  - 'B' 삭제후 T 역순으로 재배열
+ * 
  * 제한)
  * 1 ≤ S.length() ≤ 999
  * 2 ≤ T.length() ≤ 1000
@@ -17,29 +25,27 @@ class Problem12904 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //S -> T
-        //문자열 뒤에 A추가 || 문자를 뒤집고 B추가
         String S = st.nextToken();
         st = new StringTokenizer(br.readLine());
         String T = st.nextToken();
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(S);
+        StringBuilder sb = new StringBuilder(T);
 
-        int TLength = T.length();
+        int SLength = S.length();
 
-        boolean isRevers = false;
-
-        while (TLength > sb.length()) {
-            int index = sb.length() - 1;
-
-            if (sb.toString().charAt(index) == T.charAt(index)) {
-
+        while (sb.length() > SLength) {
+            char ch = sb.charAt(sb.length() - 1);
+            sb.delete(sb.length()-1, sb.length());
+            if (ch != 'A') {
+                sb.reverse();
             }
-            else {
-                sb.append('B');
-                sb.insert(0,'A');
-            }
+        }
+
+        if (S.equals(sb.toString())) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
         }
     }
 }
