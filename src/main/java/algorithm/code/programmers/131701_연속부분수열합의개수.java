f68bdbd281a630b89b292512d937fa38f7c7b395
OJY <ojy9406@naver.com>
2022-12-20 22:35:33 +0900

    프로그래머스 연속부분수열합개수 문제 추가

---
+++ src/main/java/algorithm/code/programmers/131701_연속부분수열합의개수.java
@@ -0,0 +1,21 @@
+package algorithm.code.programmers;
+
+/**
+ *
+ * 제한)
+ * 3 ≤ elements의 길이 ≤ 1,000
+ * 1 ≤ elements의 원소 ≤ 1,000
+ */
+class Lesson131701 {
+    static int solution(int[] elements) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] elements = {7,9,1,1,4};
+        System.out.println("===START===");
+        System.out.println(solution(elements));
+        System.out.println("===END===");
+    }
+}
