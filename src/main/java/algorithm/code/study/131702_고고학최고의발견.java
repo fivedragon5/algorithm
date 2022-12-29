4a5052cda806f284b607167fcda4613d58b25141
fad <ojy9406@naver.com>
2022-12-29 18:46:19 +0900

    [백준] 개똥벌레 시간초과

--- src/main/java/algorithm/code/study/131702_고고학최고의발견.java
+++ src/main/java/algorithm/code/study/131702_고고학최고의발견.java
@@ -1,9 +1,27 @@
 package algorithm.code.study;
 
+/**
+ *
+ *
+ * 제한)
+ * clockHands[n][m]
+ * 2 <= n, m <= 8
+ * 0 ≤ clockHands[n][m] ≤ 3
+ * 0:12시, 1:3시, 2:6시, 3:9시
+ * 해결 가능한 퍼즐만 주어집니다.
+ */
 class Lesson131702 {
     static int solution(int[][] clockHands) {
         int answer = 0;
         return answer;
+    }
+
+    static void rotate() {
+
+    }
+
+    static int plus(int number) {
+        return number == 3 ? 0 : number + 1;
     }
 
     public static void main(String[] args) {
@@ -13,3 +31,18 @@ class Lesson131702 {
         System.out.println("===END===");
     }
 }
+
+/*
+0 3 3 0
+3 2 2 3
+0 3 2 0
+0 3 3 3
+
+------
+
+0 1 3 0
+1 2 0 0
+3 0 2 2
+0 2 0 0x
+
+ */
--- src/main/java/algorithm/code/study/3020_개똥벌레.java
+++ src/main/java/algorithm/code/study/3020_개똥벌레.java
@@ -3,12 +3,95 @@ package algorithm.code.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 제한)
+ * N%2 == 0
+ * 2 <= N <= 200,000
+ * 2 <= H <= 500,000
+ */
 class Problem3020 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        int[] crack = new int[H];
+        int height = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            height = Integer.parseInt(st.nextToken());
+            if (i % 2 == 0) {
+                for (int j = 0; j < height; j++) {
+                    crack[j]++;
+                }
+            }
+            else {
+                for (int j = H - 1; j >= H - height; j--) {
+                    crack[j]++;
+                }
+            }
+        }
+        Arrays.sort(crack);
+        int min = crack[0];
+        int count = 0;
+        for (int wall : crack) {
+            if (min != wall) {
+                break;
+            }
+            count++;
+        }
+        System.out.println(min + " " + count);
     }
 }
+/*
+14 5
+1
+3
+4
+2
+2
+4
+3
+4
+3
+3
+3
+2
+3
+3
+
+6 7
+1
+5
+3
+3
+5
+1
+
+2 3
+
+14 5
+1
+3
+4
+2
+2
+4
+3
+4
+3
+3
+3
+2
+3
+3
+
+7 2
+ */
 
