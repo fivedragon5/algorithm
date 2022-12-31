5b9fcf8d453aa67ab0dd5476102f39232409ec4e
fad <ojy9406@naver.com>
2022-12-31 15:05:53 +0900

    [백준] 개똥벌레 문제 풀이 및 설명

--- src/main/java/algorithm/code/study/1967_트리의지름.java
+++ src/main/java/algorithm/code/study/1967_트리의지름.java
@@ -5,10 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 제한)
+ * 1 <= n <= 10000
+ */
 class Problem1967 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+
+        }
     }
 }
+/*
+12
+1 2 3
+1 3 2
+2 4 5
+3 5 11
+3 6 9
+4 7 1
+4 8 7
+5 9 15
+5 10 4
+6 11 6
+6 12 10
+
+45
+ */
 
--- src/main/java/algorithm/code/study/3020_개똥벌레.java
+++ src/main/java/algorithm/code/study/3020_개똥벌레.java
@@ -7,6 +7,10 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
+ * 접근)
+ * 석순과 종류석을 정렬
+ * 석순과 종류석을 탐색할때 시간을 단축시키기위해 시작위치를 기억해서 시간을 줄여준다.
+ *
  * 제한)
  * N%2 == 0
  * 2 <= N <= 200,000
@@ -20,34 +24,103 @@ class Problem3020 {
         int N = Integer.parseInt(st.nextToken());
         int H = Integer.parseInt(st.nextToken());
 
-        int[] crack = new int[H];
+        int[] down = new int[N/2];
+        int[] up = new int[N/2];
         int height = 0;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             height = Integer.parseInt(st.nextToken());
             if (i % 2 == 0) {
-                for (int j = 0; j < height; j++) {
-                    crack[j]++;
-                }
+                down[i/2] = height;
             }
             else {
-                for (int j = H - 1; j >= H - height; j--) {
-                    crack[j]++;
+                up[i/2] = H - height + 1;
+            }
+        }
+
+        Arrays.sort(down);
+        Arrays.sort(up);
+
+        int startDown = 0;
+        int startUp = 0;
+        int destoryCount = 0;
+
+        int nextDown = 0;
+        int nextUp = 0;
+        int min = 200000;
+        int minCount = 0;
+
+        //1구간의 장애물이 파괴한 개수
+        //석순의 길이가 1이랑 같거나 큰 개수 + 전체길이 - 종유석 높이 + 1의 길이가 1보다 큰 개수
+        for (int i = 0; i < H; i++) {
+            for (int j = nextDown; j < N/2; j++) {
+                if (down[j] >= i+1) {
+                    nextDown = j;
+                    break;
+                }
+                else {
+                    nextDown++;
                 }
             }
-        }
-        Arrays.sort(crack);
-        int min = crack[0];
-        int count = 0;
-        for (int wall : crack) {
-            if (min != wall) {
-                break;
+            destoryCount = N/2 - nextDown;
+
+            for (int j = nextUp; j < N/2; j++) {
+                if (up[j] > i+1) {
+                    break;
+                }
+                else {
+                    nextUp++;
+                }
             }
-            count++;
+            destoryCount += nextUp;
+
+            if (min == destoryCount) {
+                minCount++;
+            }
+            else if (min > destoryCount) {
+                min = destoryCount;
+                minCount = 1;
+            }
         }
-        System.out.println(min + " " + count);
+
+        System.out.println(min + " " + minCount);
     }
+//    public static void main(String args[]) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int N = Integer.parseInt(st.nextToken());
+//        int H = Integer.parseInt(st.nextToken());
+//
+//        int[] crack = new int[H];
+//        int height = 0;
+//
+//        for (int i = 0; i < N; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            height = Integer.parseInt(st.nextToken());
+//            if (i % 2 == 0) {
+//                for (int j = 0; j < height; j++) {
+//                    crack[j]++;
+//                }
+//            }
+//            else {
+//                for (int j = H - 1; j >= H - height; j--) {
+//                    crack[j]++;
+//                }
+//            }
+//        }
+//        Arrays.sort(crack);
+//        int min = crack[0];
+//        int count = 0;
+//        for (int wall : crack) {
+//            if (min != wall) {
+//                break;
+//            }
+//            count++;
+//        }
+//        System.out.println(min + " " + count);
+//    }
 }
 /*
 14 5
