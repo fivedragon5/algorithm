6528306856c74b165d4db1e92e699704ec236743
fad <ojy9406@naver.com>
2023-01-06 17:37:25 +0900

    [백준] 성싶당, 유사칸토어 비트열 다시 풀어보기

--- src/main/java/algorithm/code/study/148652_유사칸토어비트열.java
+++ src/main/java/algorithm/code/study/148652_유사칸토어비트열.java
@@ -1,9 +1,44 @@
 package algorithm.code.study;
 
+/**
+ * TODO : 다시풀기
+ * 접근)
+ *  n번째 유사 칸토열 = (n-1)유사 칸토열 + (n-1)유사 칸토열 + 5^n-1개의 0 + (n-1)유사 칸토열 + (n-1)유사 칸토열
+ * 풀이)
+ *
+ * 제한)
+ * 1 ≤ n ≤ 20
+ * 1 ≤ l, r ≤ 5n
+ * l ≤ r < l + 10,000,000
+ * l과 r은 비트열에서의 인덱스(1-base)이며 폐구간 [l, r]을 나타냅니다.
+ *
+ */
 class Lesson148652 {
     static int solution(int n, long l, long r) {
-        int answer = 0;
-        return answer;
+        long totalLength = (long) Math.pow(5, n);
+
+        long left = totalLength / l;
+        long right = totalLength / r;
+
+        System.out.println(left + "/" + right);
+
+        return 0;
+    }
+
+    static String cantor(String str, int repeatCount) {
+        String fiveZero = "00000";
+        for (int j = 2; j <= repeatCount; j++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(str);
+            sb.append(str);
+            for (int i = 0; i < Math.pow(5, j - 2); i++) {
+                sb.append(fiveZero);
+            }
+            sb.append(str);
+            sb.append(str);
+            str = sb.toString();
+        }
+        return str;
     }
 
     public static void main(String[] args) {
@@ -12,6 +47,17 @@ class Lesson148652 {
         long r = 17;
         System.out.println("===START===");
         System.out.println(solution(n, l, r));
+//        System.out.println(solution(2, l, r));
+//        System.out.println(solution(3, l, r));
+//        System.out.println(solution(4, l, r));
         System.out.println("===END===");
     }
 }
+//1 : 4^1개
+//11011
+//2: 0, 5 4^2개
+//1101111011000001101111011
+//3: 0, 5^2
+//11011110110000011011110111101111011000001101111011000000000000000000000000011011110110000011011110111101111011000001101111011
+//1101111011000001101111011110111101100000110111101100000000000000000000000001101111011000001101111011110111101100000110111101111011110110000011011110111101111011000001101111011000000000000000000000000011011110110000011011110111101111011000001101111011000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001101111011000001101111011110111101100000110111101100000000000000000000000001101111011000001101111011110111101100000110111101111011110110000011011110111101111011000001101111011000000000000000000000000011011110110000011011110111101111011000001101111011
+
--- src/main/java/algorithm/code/study/24041_성싶당밀키트.java
+++ src/main/java/algorithm/code/study/24041_성싶당밀키트.java
@@ -3,12 +3,89 @@ package algorithm.code.study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
+/**
+ * TODO: 20230106 포인터를 통해 어느 시점 이상일경우 곰팡이의 수를 누적하고 중요도 부분은 탐색? 을 사용해야 할꺼 같음.
+ *
+ * 제한)
+ * 1 <= N <= 200,000
+ * 1 <= G <= 10^9
+ * 0 <= K <= N
+ * 1 <= Si <= 10^9
+ * 1 <= Li <= 10^9
+ * O = {0,1}
+ */
 class Problem24041 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        //세균 수 : S(i) * max(1, x - L(i))
+        int[][] items = new int[N][3];
+//        ArrayList<int[]> items = new ArrayList<>();
+        // 부패속도, 유통기한, 중요도
+        int S = 0;
+        int L = 0;
+        int O = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            S = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+            O = Integer.parseInt(st.nextToken());
+            items[i][0] = S;
+            items[i][1] = L;
+            items[i][2] = O;
+        }
+
+
+        for (int[] item : items) {
+            System.out.println(Arrays.toString(item));
+        }
+
+        Arrays.sort(items, Comparator.comparingInt(i -> i[1]));
+        
+        System.out.println("정렬");
+        for (int[] item : items) {
+            System.out.println(Arrays.toString(item));
+        }
+        // 9 -> 3
+        // 10 -> 6
+        // 11 -> 16
+        // 12 -> 26
     }
 }
+// L + 36 / 2
+/*
+4 36 0
+2 14 1
+3 8 1
+5 12 1
+7 10 0
+
+12
+
+4 36 1
+2 14 1
+3 8 1
+5 12 1
+7 10 0
+
+13
+
+4 36 2
+2 14 1
+3 8 1
+5 12 1
+7 10 0
+
+14
+ */
 
