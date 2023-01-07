afcb21c687e44d72074018cc8f3ec67130e25718
fad <ojy9406@naver.com>
2023-01-07 17:20:02 +0900

    [백준] 성싶당밀키트 풀이 생각 및 구현해보기

--- src/main/java/algorithm/code/study/24041_성싶당밀키트.java
+++ src/main/java/algorithm/code/study/24041_성싶당밀키트.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 /**
- * TODO: 20230106 포인터를 통해 어느 시점 이상일경우 곰팡이의 수를 누적하고 중요도 부분은 탐색? 을 사용해야 할꺼 같음.
+ * TODO: 20230107 변수인 x일의 범위가 넓기 때문에 이분탐색을 활용하자. 추가로 필요한 재료, 필요하지 않는 재료를 정렬하고 K개를 뺀 상태에서 이분탐색을 진행하는것이 좋아보임
+ *
+ *
  *
  * 제한)
  * 1 <= N <= 200,000
@@ -40,29 +41,55 @@ class Problem24041 {
             S = Integer.parseInt(st.nextToken());
             L = Integer.parseInt(st.nextToken());
             O = Integer.parseInt(st.nextToken());
+
             items[i][0] = S;
             items[i][1] = L;
             items[i][2] = O;
         }
 
-
         for (int[] item : items) {
             System.out.println(Arrays.toString(item));
         }
 
-        Arrays.sort(items, Comparator.comparingInt(i -> i[1]));
-        
+        Arrays.sort(items, (i1, i2) -> {
+            if (i1[1] == i2[1]) {
+                return i2[0] - i1[0];
+            }
+            return i1[1] - i2[1];
+        });
+
         System.out.println("정렬");
+
         for (int[] item : items) {
             System.out.println(Arrays.toString(item));
         }
-        // 9 -> 3
-        // 10 -> 6
-        // 11 -> 16
-        // 12 -> 26
+        
+        // 시작일
+        int day = items[0][1] + 1;
+        // 부패가 시작된 배열의 인덱스
+        int pointer = 0;
+        // 현재까지의 세균 수
+        int totalGerm = 0;
+        // 늘어나는 세균 수
+        int increaseGerm = 0;
+
+        while (totalGerm <= G) {
+            for (int i = pointer; i < items.length; i++) {
+                if (items[i][1] < day) {
+                    pointer++;
+                    increaseGerm += items[i][0];
+                }
+                else {
+                    break;
+                }
+            }
+            totalGerm += increaseGerm;
+            System.out.println("day : " + day + "/ totalGerm" + totalGerm);
+            day++;
+        }
+        System.out.println(day-1);
     }
 }
-// L + 36 / 2
 /*
 4 36 0
 2 14 1
