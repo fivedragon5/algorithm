a71eb6fffe5bd6d80926328efc88ee34f264fc65
fad <ojy9406@naver.com>
2023-01-18 14:06:38 +0900

    [알고리즘 스터디] 이번주 문제 추가 및 지난주 문제 이동

---
+++ src/main/java/algorithm/code/study/15486_퇴사2.java
@@ -0,0 +1,9 @@
+package algorithm.code.study;
+
+class Problem15486 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
+
---
+++ src/main/java/algorithm/code/study/1976_여행가자.java
@@ -0,0 +1,31 @@
+package algorithm.code.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Problem1976 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
+/*
+4 2 0 0 8
+0 2
+3 4
+5 6
+7 8
+4 4 4 1 3 3 3 2
+
+0
+0
+3
+0
+0
+8
+6
+3
+ */
+
