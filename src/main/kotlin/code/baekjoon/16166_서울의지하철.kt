9107db2291bc9f23f48cf901d2a45a30634ed2f2
fad <ojy9406@naver.com>
2023-02-15 19:49:04 +0900

    서울의 지하철 풀이 추가

--- src/main/kotlin/code/baekjoon/16166_서울의지하철.kt
+++ src/main/kotlin/code/baekjoon/16166_서울의지하철.kt
@@ -1,38 +1,39 @@
 import java.util.StringTokenizer
 
-var list = arrayListOf<ArrayList<Int>>()
+var list = arrayListOf<ArrayList<Long>>()
 var nodes = arrayListOf<MutableSet<Int>>()
-var destination: Int = 0
+var destination: Long = 0L
 var destinationLines = mutableListOf<Int>()
 var visited: Array<Boolean> = emptyArray()
-var min = 12
+var min = 11
 
 fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
-    val st = StringTokenizer(readLine())
+    var st = StringTokenizer(readLine())
     val N = st.nextToken().toInt()
     var startLineList = mutableListOf<Int>()
 
     for(i in 0 until N) {
-        val temp = readLine().split(" ").map { it.toInt() }
+        st = StringTokenizer(readLine())
+        val stationCount = st.nextToken().toInt()
         list.add(ArrayList())
         nodes.add(mutableSetOf())
-        for (j in 1 .. temp[0]) {
-            if (temp[j] == 0) {
+        for (j in 1 .. stationCount) {
+            val station = st.nextToken().toLong()
+            if (station == 0L) {
                 startLineList.add(i)
             }
-            list.get(i).add(temp[j])
+            list[i].add(station)
         }
     }
 
-    destination = StringTokenizer(readLine()).nextToken().toInt()
+    destination = StringTokenizer(readLine()).nextToken().toLong()
     checkDestination()
     for (start in startLineList) {
         visited = Array(N) { false }
         dfs(start, 0)
     }
 
-
-    if (min == 12) {
+    if (min === 11) {
         print(-1)
     }
     else {
@@ -41,20 +42,23 @@ fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
 }
 
 fun checkDestination() {
-    var map = HashMap<Int, MutableSet<Int>>()
+    var map = HashMap<Long, MutableSet<Int>>()
 
     for(i in 0 until list.size) {
-        for (subwayStop in list.get(i)) {
+        for (subwayStop in list[i]) {
             val lineList = map.getOrDefault(subwayStop, mutableSetOf())
-            if (lineList.isEmpty()) {
+            if (lineList.size === 0) {
                 map[subwayStop] = mutableSetOf(i)
             }
             else {
                 for (l in lineList) {
-                    nodes[i].add(l)
-                    nodes[l].add(i)
+                    if (i !== l) {
+                        nodes[i].add(l)
+                        nodes[l].add(i)
+                    }
                 }
-                map.get(subwayStop)!!.add(i)
+                lineList.add(i)
+                map.put(subwayStop, lineList)
             }
             if (subwayStop === destination) {
                 destinationLines.add(i)
@@ -71,14 +75,16 @@ fun dfs(startLine: Int, count: Int) {
     }
     var line = startLine
     visited[line] = true
-    for (nextLine in nodes[line]) {
-        if (!visited[nextLine]) {
-            visited[nextLine] = true
-            dfs(nextLine, count + 1)
-            visited[nextLine] = false
+    if (nodes[line].size != 0) {
+        for (nextLine in nodes[line]) {
+            if (!visited[nextLine]) {
+                visited[nextLine] = true
+                dfs(nextLine, count + 1)
+                visited[nextLine] = false
+            }
         }
+        visited[line] = false
     }
-    visited[line] = false
 }
 /*
 3
@@ -106,5 +112,23 @@ fun dfs(startLine: Int, count: Int) {
 7 13 8 10 7 99 13 22
 2 13 22
 22
+
+5
+7 12 1 2 3 1 12 8
+2 12 13
+7 13 0 9 4 99 99 99
+7 13 8 10 7 99 13 22
+3 13 22 4294967296
+22
+
+3
+3 6 99 4294967295
+4 2 5 7 10
+2 10 8
+55
+
+1
+1 0
+0
  */
 
