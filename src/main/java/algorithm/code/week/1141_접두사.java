278b97e4cefa32309fab0491dee31c58fcbf9c7f
fad <fad@path.how>
2023-02-17 10:04:45 +0900

    접두사 문제 푸는중

--- src/main/java/algorithm/code/week/1141_접두사.java
+++ src/main/java/algorithm/code/week/1141_접두사.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
