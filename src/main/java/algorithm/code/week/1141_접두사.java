a7a4378425884f1b1b6abf7e92a7c6aa61b689a5
fad <ojy9406@naver.com>
2023-02-19 23:12:11 +0900

    접두사 풀이 및 설명 추가

--- src/main/java/algorithm/code/week/1141_접두사.java
+++ src/main/java/algorithm/code/week/1141_접두사.java
@@ -3,11 +3,18 @@ package algorithm.code.week;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 /**
+ * 풀이)
+ * 1. 입력받은 단어들은 글자길이 오름차순으로 정렬
+ *  - 두 단어가 주어젔을때 두 단어중 길이가 짧은 단어가 긴 단어의 접두사가 될 수 있음
+ * 2. 정렬한 단어들을 순회 하면서 현재 단어가 뒤에 있는 단어의 접두사가 될 수 있는지 확인
+ *  - 2-1 어떤 단어도 접두사가 되지 않는 경우 카운트 증가
+ *  - 2-2 접두사가 될 경우 다음 단어확인
+ *
  * 제한)
  * 1 <= N <= 50
  * 1 <= word.length <= 50
@@ -26,51 +33,28 @@ class Problem1141 {
             words[i] = st.nextToken();
         }
 
-        Arrays.sort(words);
+        Arrays.sort(words, Comparator.comparingInt(String::length));
 
-        ArrayList<ArrayList<String>> list = new ArrayList<>();
-        int index = 0;
-        list.add(new ArrayList());
-        String beforeWord = words[0];
-        list.get(0).add(beforeWord);
+        int count = 0;
 
-        for (int i = 1; i < words.length; i++) {
-            String currentWord = words[i];
-            if (currentWord.startsWith(beforeWord)) {
-                list.get(index).add(currentWord);
-            }
-            else {
-                list.add(new ArrayList());
-                list.get(++index).add(currentWord);
-                beforeWord = currentWord;
-            }
-        }
-
-        int count = list.size();
-
-        for (ArrayList<String> wordList : list) {
-            System.out.println(wordList.toString());
-            int max = 0;
-            for (int i = 0; i < wordList.size(); i++) {
-                int c = 0;
-                for (int j = i; j < wordList.size(); j++) {
-                    if (!wordList.get(i).startsWith(wordList.get(j)) && !wordList.get(j).startsWith(wordList.get(i))) {
-                        c++;
-                    }
+        for (int i = 0 ; i < N ; i++) {
+            String word = words[i];
+            boolean isFlag = false;
+            for (int j = i + 1 ; j < N ; j++) {
+                if (words[j].startsWith(word)) {
+                    isFlag = true;
+                    break;
                 }
-                max = Math.max(c, max);
             }
-            if (max > 1) {
-                count = count + max - 1;
-            }
-            else {
-                count += max;
+            if (!isFlag) {
+                count++;
             }
         }
 
         System.out.println(count);
     }
 }
+
 /*
 6
 hello
@@ -97,7 +81,7 @@ hac
 ha
 running
 
-4
+3
 
 7
 ca
@@ -117,6 +101,13 @@ bbm
 bbmiis
 vim
 aosi
+
+5
+hi
+hi
+hk
+h
+zh
 
  */
 
