6d022c57efc5a5f38c093d44e9d5c981611cb510
fad <ojy9406@naver.com>
2023-03-01 18:08:50 +0900

    [BACKJOON-1806] 부분합 문제 class명 변경

--- src/main/java/algorithm/code/week/1806_부분합.java
+++ src/main/java/algorithm/code/week/1806_부분합.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Problem2504 {
+class Problem1806 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
