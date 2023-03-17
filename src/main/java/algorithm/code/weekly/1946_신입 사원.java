d8d2cab36a710f95c7c1bc24be2b7f89e0c715dc
fad <fad@path.how>
2023-03-17 19:00:11 +0900

    [BAEKJOON-1946] 신입사원 문제 푸는중

--- src/main/java/algorithm/code/weekly/1946_신입 사원.java
+++ src/main/java/algorithm/code/weekly/1946_신입 사원.java
@@ -5,10 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 제한)
+ * 1 <= T <= 20
+ * 1 <= N <= 100,000
+ */
 class Problem1946 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
     }
 }
+
+/*
+2
+5
+3 2
+1 4
+4 1
+2 3
+5 5
+7
+3 6
+7 3
+4 2
+1 4
+5 7
+2 5
+6 1
+
+4
+3
+ */
 
