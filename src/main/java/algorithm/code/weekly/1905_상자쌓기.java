f23117476e27dd3e152e68a3852922b89bd1a380
fad <fad@path.how>
2023-03-30 18:08:33 +0900

    [BACKJOON-23254] 나는 기말 고사형 인간이야 추가

--- src/main/java/algorithm/code/weekly/1905_상자쌓기.java
+++ src/main/java/algorithm/code/weekly/1905_상자쌓기.java
@@ -5,10 +5,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1 ≤ Lx, Ly ≤ 1,000
+ * 1 ≤ N ≤ 20,000
+ * 1 ≤ lx
+ * 0 ≤ px
+ * px+lx ≤ Lx
+ * 1 ≤ ly
+ * 0 ≤ py
+ * py+ly ≤ Ly
+ * 1 ≤ lz ≤ 100,000
+ */
 class Problem1905 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int Lx = Integer.parseInt(st.nextToken()); // 가로
+        int Ly = Integer.parseInt(st.nextToken()); // 세로
+        int N = Integer.parseInt(st.nextToken()); // 상자갯수
+        // lx, ly, lz, px, py
+        // 가로, 세로, 높이,
+
+        int[][] array = new int[1000][1000];
+
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int z = Integer.parseInt(st.nextToken()) - 1;
+            int dx = Integer.parseInt(st.nextToken()) - 1;
+            int dy = Integer.parseInt(st.nextToken()) - 1;
+
+            for (int j = dx; j < dx + x; j++) {
+
+            }
+
+        }
     }
 }
-
+/*
+7 5 4
+4 3 2 0 0
+3 3 1 3 0
+7 1 2 0 3
+2 3 3 2 2
+*/
--- src/main/java/algorithm/code/weekly/23254_나는 기말고사형 인간이야.java
+++ src/main/java/algorithm/code/weekly/23254_나는 기말고사형 인간이야.java
@@ -3,12 +3,82 @@ package algorithm.code.weekly;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * 제한)
+ *  1 <= N, M <= 200,000
+ *  1 <= a, b <= 100
+ */
 class Problem23254 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int hours = N = 24;
+
+        int[][] score = new int[2][M];
+        List<Subject> subjectList = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                score[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            subjectList.add(new Subject(score[0][i], score[1][i]));
+        }
+
+        Collections.sort(
+            subjectList,
+            (o1, o2) -> {
+                if (o2.up == o1.up) {
+                    return o1.score - o2.score;
+                }
+                return o2.up - o1.up;
+            }
+        );
+
+        for (Subject s : subjectList) {
+            System.out.println(s.toString());
+        }
+    }
+
+    static class Subject {
+        int score;
+        int up;
+
+        Subject(int score, int up) {
+            this.score = score;
+            this.up = up;
+        }
+
+        @Override
+        public String toString() {
+            return this.score + ", " + this.up;
+        }
     }
 }
+/*
+1 2
+50 60
+4 3
+
+194
+
+8 7
+30 15 70 50 40 40 50
+2 2 1 3 1 2 1
+
+627
+ */
 
