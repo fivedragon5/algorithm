de2b0baea3055e0126d2380f7b778053de519fb5
fad <ojy9406@naver.com>
2023-04-02 22:12:05 +0900

    [BACKJOON-23254] 나는 기말고사형 인간이야 문제 풀이 및 설명 추가

--- src/main/java/algorithm/code/weekly/23254_나는 기말고사형 인간이야.java
+++ src/main/java/algorithm/code/weekly/23254_나는 기말고사형 인간이야.java
@@ -7,6 +7,15 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
+ * 접근)
+ *  1. 우선순위 큐
+ *
+ * 풀이)
+ *  1. 우선순위 큐에 현재 점수, 시간당 점수 정보를 시간당 점수 내림차순으로 넣는다
+ *  2. 큐에서 하나씩 빼면서 주어신 시간당 가장 높은 점수를 올릴 수 있게 계산 후
+ *     시간당 점수를 갱신하여 다시 우선순위 큐에 넣는다.
+ *  3. 점수를 갱신하면서 시간을 누적함
+ *
  * 제한)
  *  1 <= N, M <= 200,000
  *  1 <= a, b <= 100
@@ -18,6 +27,7 @@ class Problem23254 {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int answer = 0;
 
         int hours = N * 24;
 
@@ -32,10 +42,11 @@ class Problem23254 {
         }
 
         for (int i = 0; i < M; i++) {
+            answer += score[0][i];
             pq.add(new Subject(score[0][i], score[1][i]));
         }
 
-        while (hours < 0) {
+        while (hours > 0 && !pq.isEmpty()) {
             Subject subject = pq.poll();
             int maxTime = (100 - subject.score) / subject.up;
             if (maxTime <= hours) {
@@ -43,10 +54,16 @@ class Problem23254 {
             }
             else {
                 maxTime = hours;
+                hours = 0;
             }
             int changeUp = (100 - subject.score) % subject.up;
-            pq.add(new Subject(subject.score + (subject.up * maxTime), changeUp));
+            int upScore = subject.up * maxTime;
+            answer += upScore;
+            if (upScore + subject.score != 100) {
+                pq.add(new Subject(subject.score + upScore, changeUp));
+            }
         }
+        System.out.println(answer);
     }
 
     static class Subject {
@@ -56,11 +73,6 @@ class Problem23254 {
         Subject(int score, int up) {
             this.score = score;
             this.up = up;
-        }
-
-        @Override
-        public String toString() {
-            return this.score + "," + this.up;
         }
     }
 }
