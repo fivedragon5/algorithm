b3399a5686533e76899bd88123d1d25184fc6b22
fad <ojy9406@naver.com>
2023-04-04 00:53:15 +0900

    [BACKJOON-1905] 상자쌓기 다시 풀어보자

--- src/main/java/algorithm/code/weekly/1905_상자쌓기.java
+++ src/main/java/algorithm/code/weekly/1905_상자쌓기.java
@@ -3,7 +3,9 @@ package algorithm.code.weekly;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
@@ -22,41 +24,82 @@ class Problem1905 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int Lx = Integer.parseInt(st.nextToken()); // 가로
-        int Ly = Integer.parseInt(st.nextToken()); // 세로
-        int N = Integer.parseInt(st.nextToken()); // 상자갯수
+        int Lx = Integer.parseInt(st.nextToken());
+        int Ly = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        int[][] layer = new int[Ly][Lx];
-
-        int max = 0;
-        int answer = 0;
+        int[][] layer = new int[Ly + 1][Lx + 1];
+        int[][] boxes = new int[N][5];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int z = Integer.parseInt(st.nextToken());
-            int dx = Integer.parseInt(st.nextToken());
-            int dy = Integer.parseInt(st.nextToken());
+            boxes[i][0] = Integer.parseInt(st.nextToken());
+            boxes[i][1] = Integer.parseInt(st.nextToken());
+            boxes[i][2] = Integer.parseInt(st.nextToken());
+            boxes[i][3] = Integer.parseInt(st.nextToken()) + 1;
+            boxes[i][4] = Integer.parseInt(st.nextToken()) + 1;
+        }
 
-            max = 0;
+        PriorityQueue<Box> pq = new PriorityQueue<>((o1, o2) -> o2.currentHeight - o1.currentHeight);
+        pq.add(new Box(boxes[0][0], boxes[0][1], boxes[0][3], boxes[0][4], boxes[0][2]));
 
-            for(int k = dy; k < dy + y; k++) {
-                for (int j = dx; j < dx + x; j++) {
-                    max = Math.max(max, layer[k][j] + z);
+        for (int i = 1; i < N; i++) {
+            Box newBox = new Box(boxes[i][0], boxes[i][1], boxes[i][3], boxes[i][4], boxes[i][2]);
+            List<Box> list = new LinkedList();
+            while(!pq.isEmpty()) {
+                Box box = pq.poll();
+                if (check(box, newBox)) {
+                    newBox.currentHeight += box.currentHeight;
+                    pq.add(newBox);
+                    pq.add(box);
+                    break;
+                }
+                else {
+                    list.add(box);
                 }
             }
-            for(int k = dy; k < dy + y; k++) {
-                for (int j = dx; j < dx + x; j++) {
-                    layer[k][j] += max;
-                }
+            if (pq.size() == 0) pq.add(newBox);
+            for (Box box : list) {
+                pq.add(box);
             }
-            answer = Math.max(answer, max + z);
         }
-        for (int[] a : layer) {
-            System.out.println(Arrays.toString(a));
+        System.out.println(pq.poll().currentHeight);
+    }
+
+    static boolean check(Box one, Box two) {
+        boolean flag = false;
+        for (int y = two.dy; y < two.dy + two.y; y++) {
+            if (y >= one.dy && y <= one.dy + y) {
+                flag = true;
+                break;
+            }
         }
-        System.out.println(answer);
+
+        if(!flag) return false;
+
+        for (int x = two.dx; x < two.dx + two.x; x++) {
+            if (x >= one.dx && x <= one.dx + x) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static class Box {
+        int x;
+        int y;
+        int dx;
+        int dy;
+        int currentHeight;
+
+        public Box(int x, int y, int dx, int dy, int currentHeight) {
+            this.x = x;
+            this.y = y;
+            this.dx = dx;
+            this.dy = dy;
+            this.currentHeight = currentHeight;
+        }
     }
 }
 /*
@@ -66,3 +109,42 @@ class Problem1905 {
 7 1 2 0 3
 2 3 3 2 2
 */
+
+//    public static void main(String args[]) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int Lx = Integer.parseInt(st.nextToken());
+//        int Ly = Integer.parseInt(st.nextToken());
+//        int N = Integer.parseInt(st.nextToken());
+//
+//        int[][] layer = new int[Ly+1][Lx+1];
+//
+//        int answer = 0;
+//
+//        for (int i = 0; i < N; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            int x = Integer.parseInt(st.nextToken());
+//            int y = Integer.parseInt(st.nextToken());
+//            int z = Integer.parseInt(st.nextToken());
+//            int dx = Integer.parseInt(st.nextToken()) + 1;
+//            int dy = Integer.parseInt(st.nextToken()) + 1;
+//
+//            int max = 0;
+//
+//            for(int k = dy; k < dy + y; k++) {
+//                for (int j = dx; j < dx + x; j++) {
+//                    max = Math.max(max, layer[k][j] + z);
+//                }
+//            }
+//            for(int k = dy; k < dy + y; k++) {
+//                for (int j = dx; j < dx + x; j++) {
+//                    layer[k][j] = max;
+//                }
+//            }
+//            answer = Math.max(answer, max);
+//        }
+//        System.out.println(answer);
+//    }
+//}
+
