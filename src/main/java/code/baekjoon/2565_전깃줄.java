d95387951e3b5c70ce9e270c8c08c79c124d5226
fad <fad@path.how>
2024-05-01 23:37:52 +0900

    [BAEKJOON-2465] DP 전깃줄 문제푸는중

---
+++ src/main/java/code/baekjoon/2565_전깃줄.java
@@ -0,0 +1,74 @@
+/**
+ *
+ * https://www.acmicpc.net/problem/2565
+ * 문제)
+ * N : 구멍의 갯수
+ * M : 부피
+ * 제한)
+ * 1 <= 전깃줄 <= 100
+ * 1 <= 위치 <= 500
+ */
+
+package code.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+class Problem2565 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int answer = 0;
+
+        int telephonePoleCount = Integer.parseInt(st.nextToken());
+
+        int[][] list = new int[telephonePoleCount][2];
+        int[] dp = new int[101];
+
+        for (int i = 0; i < telephonePoleCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
+            list[i][0] = first;
+            list[i][1] = second;
+        }
+
+        Arrays.sort(list, (a1, a2) -> {return a1[0] - a2[0];});
+
+        for (int i = 1; i < telephonePoleCount; i++) {
+            dp[i] = 1;
+            for (int j = 1; j < telephonePoleCount; j++) {
+                if (list[i][1] < list[j][1]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            answer = max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
+/*
+8
+1 8
+3 9
+2 2
+4 1
+6 4
+10 10
+9 7
+7 6
+
+3
+ */
+
+
+
