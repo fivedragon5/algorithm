61fe5271465e1d417bb4ed988434589b879eebc7
fad <ojy9406@naver.com>
2024-11-22 00:23:03 +0900

    [BAEKJOON-1244] 스위치 켜고 끄기 문제 풀이 및 해설 추가

--- src/main/java/code/baekjoon/스위치 켜고 끄기_1244.java
+++ src/main/java/code/baekjoon/스위치 켜고 끄기_1244.java
@@ -13,11 +13,22 @@ import java.util.StringTokenizer;
 /**
  * https://www.acmicpc.net/problem/1244
  * 제한)
+ * 1 <= 스위치 수 <= 100
+ * 1 <= 학생 수 <= 100
  *
  * 문제)
+ *  스위치 상태
+ *   - 1 : ON
+ *   - 2 : OFF
+ *   1. 남학생은 스위치 번호가 자기가 받은 수의 배수이면, 그 스위치의 상태를 바꾼다
+ *   2. 여학생은 자기가 받은 수와 같은 번호가 붙은 스위치를 중심으로 좌우가 대칭이면서 가장 많은 스위치를 포함하는 구간을 찾아서,
+ *   그 구간에 속한 스위치의 상태를 모두 바꾼다. 이때 구간에 속한 스위치 개수는 항상 홀수가 된다.
  *
  * 풀이)
- *
+ *  - 구현
+ *  1. 성별에 따라 분기
+ *  2. 여학생일 경우, ArrayIndexOutOfBounds 주의
+ *   - 여학생의 위치 기준으로 양 옆의 스위치를 변경해준다.
  */
 class Problem1244 {
 
@@ -59,19 +70,23 @@ class Problem1244 {
         int position = testCase[1];
         // 남학생
         if (testCase[0] == 1) {
-            for (int i = position; i < switchList.length - 1; i += position) {
+            for (int i = position; i <= switchList.length; i += position) {
                 switchList[i - 1] = toggleSwitch(switchList[i - 1]);
             }
         }
         // 여학생
         else {
             switchList[position - 1] = toggleSwitch(switchList[position - 1]);
-            for (int i = 1; i < switchList.length / 2; i++) {
+            for (int i = 1; i <= switchList.length / 2; i++) {
                 if (position - 1 - i < 0 || position - 1 + i > switchList.length - 1) {
                     break;
                 }
-                switchList[position - 1 - i] = toggleSwitch(switchList[position - 1 - i]);
-                switchList[position - 1 + i] = toggleSwitch(switchList[position - 1 + i]);
+                if (switchList[position - 1 - i] == switchList[position - 1 + i]) {
+                    switchList[position - 1 - i] = toggleSwitch(switchList[position - 1 - i]);
+                    switchList[position - 1 + i] = toggleSwitch(switchList[position - 1 + i]);
+                } else {
+                    break;
+                }
             }
         }
     }
@@ -116,8 +131,25 @@ class Problem1244 {
 
 1 0 0 0 1 1 0 1
 
-45
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-1
+4
+1 0 0 1
+3
 1 3
+2 2
+1 1
+
+1
+1
+2
+1 1
+2 1
+
+8
+0 1 0 1 0 0 0 1
+5
+1 3
+2 3
+2 5
+1 6
+1 7
  */
