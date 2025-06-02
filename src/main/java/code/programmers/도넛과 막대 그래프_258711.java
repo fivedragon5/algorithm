c5c994b46348aa7f5c0329a9d05f2ed23dc31e22
fad <ojy9406@naver.com>
2025-06-02 22:56:25 +0900

    [PROGRAMMERS-258711] 프로그래머스 도넛과 막대 그래프 풀이

--- src/main/java/code/programmers/도넛과 막대 그래프_258711.java
+++ src/main/java/code/programmers/도넛과 막대 그래프_258711.java
@@ -1,12 +1,8 @@
 package code.programmers;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -37,17 +33,43 @@ import java.util.Set;
  *   - 생성된 정점은 각 그래프들과 연결되어야 함으로 생성된 정점의 나가는 간선은 2개 이상
  *  특징으로 생성된 정점을 찾고 시작하기
  *
+ *   1. 생성된 정점 찾기
+ *    - 생성된 정점의 out 간선의 개수는 모든 그래프의 모양의 총 합과 같음
+ *   2. 생성된 정점을 제외한 모든 정점들의 in, out 간선의 개수를 구하기
+ *   3. 막대, 8자 모양 그래프의 수를 구하기
+ *    - 8자 모양 : 정점의 out 간선의 개수가 2개인 정점의 수
+ *    - 막대 모양 : 정점의 out 간선의 개수가 0개인 정점의 수
+ *   4. 도넛 모양 그래프의 수 : 전체 정점의 수 - 막대 모양 그래프의 수 - 8자 모양 그래프의 수
+ *
  */
 class Lesson258711 {
     public static void main(String[] args) {
-        int[][] edges = new int[][]{{2, 3}, {4, 3}, {1, 1}, {2, 1}};
-        System.out.println(solution(edges));
+        // [2, 2], [3, 2], [3, 1], [1, 4]
+        int[][] edges5 = new int[][]{{2, 2}, {3, 2}, {3, 1}, {1, 4}};
+        System.out.println(solution(edges5));
+
+        //[4, 11], [1, 12], [8, 3], [12, 7], [7, 11], [4, 8], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8]
+        int[][] edges4 = new int[][]{
+                {1, 12}, {8, 3}, {12, 7}, {7, 11}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8},
+                {4, 11}, {4, 8}
+        };
+        System.out.println(solution(edges4));
+
+        // [1, 12], [8, 3], [12, 7], [7, 11], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8], [4, 11], [4, 8]
+        int[][] edges3 = new int[][]{
+                {1, 12}, {8, 3}, {12, 7}, {7, 11}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8},
+                {4, 11}, {4, 8}
+        };
+        System.out.println(solution(edges3));
+
+//        int[][] edges = new int[][]{{2, 3}, {4, 3}, {1, 1}, {2, 1}};
+//        System.out.println(solution(edges));
 
         int[][] edges2 = new int[][]{{4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}};
         System.out.println(solution(edges2));
     }
 
-    private static Set<Integer> nodeGroup = new HashSet<>();
+    private static int[][] NODE_INFO = new int[1000001][2];
 
     public static int[] solution(int[][] edges) {
         int createdNodeNumber = getCreatedNodeNumber(edges);
@@ -57,74 +79,59 @@ class Lesson258711 {
             return new int[]{-1, 0, 0, 0}; // 생성된 정점이 없는 경우
         }
 
-        Map<Integer, List<Integer>> graph = getGraph(edges, createdNodeNumber);
-
-
-        int[] count = checkGraph(graph);
-
+        int[] result = calcGraph(edges, createdNodeNumber);
         return new int[]{
                 createdNodeNumber,
-                count[0], // 도넛 모양 그래프 수
-                count[1], // 막대 모양 그래프 수
-                count[2]  // 8자 모양 그래프 수
+                result[0], // 도넛 모양 그래프 수
+                result[1], // 막대 모양 그래프 수
+                result[2]  // 8자 모양 그래프 수
         };
     }
 
-    private static int[] checkGraph(Map<Integer, List<Integer>> graph) {
-        boolean[] visited = new boolean[1000001];
-        int totalCount = nodeGroup.size();
-
-        int donutCount = 0;
-        int stickCount = 0;
-        int eightCount = 0;
-
-        for (int start : nodeGroup) {
-            Map<Integer, Integer> inDegreeCount = new HashMap<>();
-            Map<Integer, Integer> outDegreeCount = new HashMap<>();
-            Queue<Integer> queue = new ArrayDeque<>();
-            if (!visited[start]) {
-
-            }
-        }
-
-        return new int[]{donutCount, stickCount, eightCount};
-    }
-
-    private static Map<Integer, List<Integer>> getGraph(int[][] edges, int excludedNode) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+    private static int[] calcGraph(int[][] edges, int excludedNode) {
+        Set<Integer> nodeSet = new HashSet<>();
+        int nodeGroupCount = 0;
         for (int i = 0; i < edges.length; i++) {
             int a = edges[i][0];
             int b = edges[i][1];
-            if (a != excludedNode && b != excludedNode) {
-                List<Integer> list = graph.get(a);
-                if (list == null) {
-                    list = new ArrayList<>();
-                    list.add(b);
-                    graph.put(a, list);
-                } else {
-                    list.add(b);
-                }
-            } else if (a == excludedNode){
-                nodeGroup.add(b);
+            if (a != excludedNode) {
+                NODE_INFO[b][0]++;
+                NODE_INFO[a][1]++;
+                nodeSet.add(a);
+                nodeSet.add(b);
+            } else {
+                nodeSet.add(b);
+                nodeGroupCount++;
             }
         }
-        return graph;
+        int totalNodeCount = nodeGroupCount;
+        int donutGraphCount = 0;
+        int stickGraphCount = 0;
+        int eightGraphCount = 0;
+
+        for (int node : nodeSet) {
+            if (NODE_INFO[node][1] == 2) {
+                eightGraphCount++;
+            } else if (NODE_INFO[node][1] == 0) {
+                stickGraphCount++;
+            }
+        }
+        donutGraphCount = totalNodeCount - stickGraphCount - eightGraphCount;
+
+        return new int[]{donutGraphCount, stickGraphCount, eightGraphCount};
     }
 
     private static int getCreatedNodeNumber(int[][] edges) {
         Map<Integer, Integer> nodeLineCount = new HashMap<>();
-        boolean[] visited = new boolean[1000001];
+        HashSet<Integer> excludedNodes = new HashSet<>();
         for (int i = 0; i < edges.length; i++) {
             int a = edges[i][0];
             int b = edges[i][1];
-            if (!visited[a]) {
-                nodeLineCount.put(a, nodeLineCount.getOrDefault(a, 0) + 1);
-                nodeLineCount.remove(b);
-            }
-            visited[b] = true;
+            nodeLineCount.put(a, nodeLineCount.getOrDefault(a, 0) + 1);
+            excludedNodes.add(b);
         }
         for (int node : nodeLineCount.keySet()) {
-            if (nodeLineCount.get(node) >= 2) {
+            if (nodeLineCount.get(node) >= 2 && !excludedNodes.contains(node)) {
                 return node;
             }
         }
